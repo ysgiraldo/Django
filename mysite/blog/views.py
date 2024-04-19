730c3aeef1bb0ac39e42d7859a9f20bc08ee26a7
ysgiraldo <ygiraldoca@unal.edu.co>
2024-04-18 20:21:58 -0500

    retrieving posts by similarity

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -6,6 +6,7 @@ from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 
 # Create your views here.
 
@@ -74,15 +75,25 @@ def post_detail(request, year, month, day, post):
                             publish__year=year,
                             publish__month=month,
                             publish__day=day)
+    
     # List of active comments for this post
     comments = post.comments.filter(active=True)
+    
     # Form for users to comment
     form = CommentForm()
+    
+    # List of similar posts
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                  .exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
+                                 .order_by('-same_tags','-publish')[:4]
     return render(request,
                 'blog/post/detail.html',
                 {'post': post,
                 'comments': comments,
-                'form': form})
+                'form': form,
+                'similar_posts': similar_posts})
 
 @require_POST
 def post_comment(request, post_id):
