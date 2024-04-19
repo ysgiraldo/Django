51cb6a17c1102122bb1fac125b73befb485150ac
ysgiraldo <ygiraldoca@unal.edu.co>
2024-04-19 16:40:36 -0500

    Creating a template filter to support Markdown syntax

--- mysite/blog/templatetags/blog_tags.py
+++ mysite/blog/templatetags/blog_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from ..models import Post
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 
 register = template.Library()
 # this returns the number of posts published in the blog
@@ -19,3 +21,7 @@ def get_most_commented_posts(count=5):
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+@register.filter(name='markdown')
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))
