d0a1cc47220867c5a1e91d65050bed46b254e88c
ysgiraldo <ygiraldoca@unal.edu.co>
2024-04-19 20:17:39 -0500

    Creating feeds for blog posts

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -6,6 +6,7 @@ keyword or positional arguments.'''
 
 from django.urls import path
 from . import views
+from .feeds import LatestPostsFeed
 
 
 app_name = 'blog'
@@ -15,15 +16,11 @@ urlpatterns = [
     # https://localhost:8000/blog/ # esta url va a caer en esta condicion
     path('', views.post_list, name='post_list'),
     # path('', views.PostListView.as_view(), name='post_list'), 
-    path('tag/<slug:tag_slug>/',
-        views.post_list, name='post_list_by_tag'),
+    path('tag/<slug:tag_slug>/', views.post_list, name='post_list_by_tag'),
     # https://localhost:8000/blog/7 # detalle del post con id 7
     # path('<int:id>/', views.post_detail, name='post_detail'),
-    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
-        views.post_detail,
-        name='post_detail'),
-    path('<int:post_id>/share/',
-        views.post_share, name='post_share'),
-    path('<int:post_id>/comment/',
-        views.post_comment, name='post_comment'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.post_detail, name='post_detail'),
+    path('<int:post_id>/share/', views.post_share, name='post_share'),
+    path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
